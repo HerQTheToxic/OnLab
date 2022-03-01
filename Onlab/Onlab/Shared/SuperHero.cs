b6bf5304bbe382f8b7e25bcb933243ea318a5759
HerQTheToxic <herkules.marton.gergely@gmail.com>
2022-03-01 23:58:25 +0100

    Alapok

---
+++ Onlab/Onlab/Shared/SuperHero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onlab.Shared
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string HeroName { get; set; } = String.Empty;
+
+        //itt rendelem hozza a comicot
+        public Comic? Comic { get; set; }
+
+        //comic nezeshez jo
+        public int ComicId { get; set; }
+    }
+}
