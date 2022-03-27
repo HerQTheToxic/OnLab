41d2da6ba2d4354d695a580b7e39a49fdd362b7b
HerQTheToxic <herkules.marton.gergely@gmail.com>
2022-03-27 17:58:28 +0200

    SyncFusion

--- Mozi/Mozi/Client/Program.cs
+++ Mozi/Mozi/Client/Program.cs
@@ -3,6 +3,7 @@ global using Mozi.Client.Services.TeremService;
 global using Mozi.Client.Services.SzekService;
 global using Mozi.Client.Services.SzineszService;
 global using Mozi.Shared;
+global using Syncfusion.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Mozi.Client;
@@ -12,6 +13,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTkzNTg3QDMxMzkyZTM0MmUzMERFNEkrdWRvNTl3UDE5ZEJzVzVTa3AyWlRrdlk2TXN0clhkNnlKUjlJNDQ9");
+//kéne license
+builder.Services.AddSyncfusionBlazor();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IFilmService, FilmService>();
 builder.Services.AddScoped<ITeremService, TeremService>();
