c25c90db73d6236fe411686e73cd7d4627a5119e
HerQTheToxic <herkules.marton.gergely@gmail.com>
2022-03-29 20:59:38 +0200

    Működő nav menü

--- Mozi/Mozi/Client/Program.cs
+++ Mozi/Mozi/Client/Program.cs
@@ -17,9 +17,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTkzNTg3QDMxMzkyZTM0MmUzMERFNEkrdWRvNTl3UDE5ZEJzVzVTa3AyWlRrdlk2TXN0clhkNnlKUjlJNDQ9");
-//kéne license
-builder.Services.AddSyncfusionBlazor();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//kéne license
+builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
 builder.Services.AddScoped<IFilmService, FilmService>();
 builder.Services.AddScoped<ITeremService, TeremService>();
 builder.Services.AddScoped<ISzekService, SzekService>();
